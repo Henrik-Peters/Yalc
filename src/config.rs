792f56ec26e5b0e68cd1ca0e9a5d36d01f61c5f3
Henrik-Peters <peters-henrik@outlook.de>
2025-06-07 19:28:47 +0200

    Config attributes

--- src/config.rs
+++ src/config.rs
@@ -10,12 +10,50 @@ pub mod toml_writer;
 
 pub use config_commands::*;
 
-/// Represents the config for an executing of the yalc cleanup
+/// Represents the config for an execution of the yalc cleanup
 #[derive(Debug)]
 pub struct Config {
-    /// List with all file paths where log files should be cleaned
-    file_list: Vec<String>,
-
     /// If set to true operations will be logged but not executed
-    dry_run: bool,
+    pub dry_run: bool,
+
+    /// Which mode should be evaluated to decide whether
+    /// a file should be cleaned up or not
+    pub mode: CleanUpMode,
+
+    /// Number of files that are kept when a file rotation takes place.
+    /// If this number is exceeded, the oldest file is deleted
+    pub keep_rotate: u64,
+
+    /// List with all file paths where log files should be processed
+    pub file_list: Vec<String>,
+
+    /// Configuration of the conditions that are checked
+    /// for each file before a rotation is started
+    pub retention: RetentionConfig,
+}
+
+/// Enum representing different ways to check if a file has to be cleaned up
+#[derive(Debug)]
+pub enum CleanUpMode {
+    /// A file is cleaned up as soon as the file size
+    /// from 'retention.file_size_mb' has been exceeded
+    FileSize,
+
+    /// A file is cleaned up as soon as the last write
+    /// operation is older than (now-'retention.last_write_h')
+    LastWrite,
+
+    /// All cleanup modes are evaluated. A file is cleaned up
+    /// if at least one condition is met (OR combination)
+    All,
+}
+
+/// Represents the config values before a file cleanup should be started
+#[derive(Debug)]
+pub struct RetentionConfig {
+    /// Size in megabytes that a file must exceed in order to be cleaned up
+    file_size_mb: u64,
+
+    /// Hours since the last write operation before a file is cleaned up
+    last_write_h: u64,
 }

ad35b85f796df1f2c0af292e74104a891090ec6e
Henrik-Peters <peters-henrik@outlook.de>
2025-06-23 23:00:00 +0200

    Types for toml parser

--- src/config/toml_parser.rs
+++ src/config/toml_parser.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::ErrorKind;
@@ -34,4 +35,74 @@ pub fn load_config(path: &Path) -> Result<Config, io::Error> {
 fn load_config_file_lines(path: &Path) -> Result<String, io::Error> {
     let content: String = fs::read_to_string(path)?;
     Ok(content)
+}
+
+/// The root table of the toml file (outside of any section)
+pub type TopLevelTable = Table;
+
+/// Toml collection of key-value pairs - we use HashMap collection
+pub type Table = HashMap<Key, Value>;
+
+/// Name or identifier of the key-value pair
+type Key = String;
+
+#[derive(Debug, PartialEq)]
+pub enum Value {
+    /// Represents text with a String
+    String(String),
+
+    /// Represents whole numbers
+    Integer(i64),
+
+    /// Represents floating point numbers
+    Float(f64),
+
+    /// Represents true or false
+    Bool(bool),
+
+    /// Represents Timestamps - simple ISO 8601 string assumed
+    DateTime(String),
+
+    /// Represents an indexed list with values
+    Array(Vec<Value>),
+
+    /// Represents a toml collection of key-value pairs
+    Table(Table),
+}
+
+pub struct Parser {
+    /// Vector with all toml tokens provided by the lexer
+    tokens: Vec<Token>,
+
+    /// Index of the next token that will be processed
+    pos: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            tokens: tokens,
+            pos: 0,
+        }
+    }
+
+    /// Retrieves the next token from the input token list
+    ///
+    /// The 'next_token()' function returns the next token which
+    /// should be processed by the parser. The pos index is used
+    /// to find the next token in the input list.
+    ///
+    /// # Returns
+    /// - `Some(&Token)`: The next token from the token list.
+    /// - `None`: When the end of the token list has been reached.
+    ///
+    fn next_token(&mut self) -> Option<&Token> {
+        if self.pos < self.tokens.len() {
+            let next_token = &self.tokens[self.pos];
+            self.pos += 1;
+            Some(next_token)
+        } else {
+            None
+        }
+    }
 }

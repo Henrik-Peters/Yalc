90806a1be45662a3a1e75abca0b3bfcbfaefb282
Henrik-Peters <peters-henrik@outlook.de>
2025-07-22 23:03:23 +0200

    Config getter functions

--- src/config/config_parser.rs
+++ src/config/config_parser.rs
@@ -16,22 +16,63 @@ pub fn parse_config(root: &TopLevelTable) -> Result<Config, io::Error> {
     //Get all attributes at the root level
     let dry_run: bool = get_bool(&root, "dry_run")?;
 
-    println!("{:?}", dry_run);
+    //retention
+    let file_size_mb: u64 = get_uint(&root, "keep_rotate")?;
+
+    println!("dry_run: {:?}", dry_run);
+    println!("file_size_mb: {:?}", file_size_mb);
 
     Err(io::Error::new(ErrorKind::Other, "Not implemented"))
 }
 
-/// Helper function to extract a boolean value from the config
-fn get_bool(table: &Table, key: &str) -> Result<bool, io::Error> {
+fn get_value<'a>(table: &'a Table, key: &str) -> Result<&'a Value, io::Error> {
     match table.get(key) {
-        Some(Value::Bool(b)) => Ok(*b),
-        Some(_) => Err(io::Error::new(
-            ErrorKind::InvalidData,
-            format!("Expected boolean for config key: '{}'", key),
-        )),
+        Some(value) => Ok(value),
         None => Err(io::Error::new(
             ErrorKind::NotFound,
             format!("Missing required config key: '{}'", key),
+        )),
+    }
+}
+
+/// Helper function to extract a boolean value
+fn get_bool(table: &Table, key: &str) -> Result<bool, io::Error> {
+    match get_value(&table, &key)? {
+        Value::Bool(b) => Ok(*b),
+        _ => Err(io::Error::new(
+            ErrorKind::InvalidData,
+            format!("Expected boolean for config key: '{}'", key),
+        )),
+    }
+}
+
+/// Helper function to extract an unsigned integer value
+fn get_uint<T>(table: &Table, key: &str) -> Result<T, io::Error>
+where
+    T: Copy + TryFrom<usize>,
+{
+    match get_value(table, key)? {
+        Value::Integer(i) => {
+            if *i >= 0 {
+                let value = *i as usize;
+
+                //Try to perform conversion to the final type
+                T::try_from(value).map_err(|_| {
+                    io::Error::new(
+                        ErrorKind::InvalidData,
+                        format!("Value for '{}' exceeds the maximum allowed value", key),
+                    )
+                })
+            } else {
+                Err(io::Error::new(
+                    ErrorKind::InvalidData,
+                    format!("Negative value is not allowed for config key: '{}'", key),
+                ))
+            }
+        }
+        _ => Err(io::Error::new(
+            ErrorKind::InvalidData,
+            format!("Expected unsigned integer for config key: '{}'", key),
         )),
     }
 }

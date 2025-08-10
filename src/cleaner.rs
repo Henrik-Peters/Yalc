2128a2e827664ae1facf8a445bb91c095d9dad70
Henrik-Peters <peters-henrik@outlook.de>
2025-08-10 18:37:04 +0200

    Cleanup task stats and logs

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -8,41 +8,66 @@ use std::io;
 
 use crate::config::Config;
 
+/// Run all cleanup tasks for a given yalc config
 pub fn run_cleanup(config: &Config) -> Result<(), io::Error> {
     //Log the execution start for the cleanup
     println!(
         "Starting cleanup tasks for: {} files",
         config.file_list.len()
     );
+    println!("----------------");
+
+    //Task status counter
+    let mut tasks_executed: usize = 0;
+    let mut tasks_success: usize = 0;
+    let mut tasks_failure: usize = 0;
 
     //Check if the file list is empty
     if config.file_list.is_empty() {
         println!("File list is empty - nothing to do");
-        Ok(())
     } else {
         //Run the cleanup task for each individual file
         for (idx_task, file) in config.file_list.iter().enumerate() {
-            println!("[{}] Running task for: {}", idx_task, file);
+            let task_nr = idx_task + 1;
+            println!("[{}] Running task for: {}", task_nr, file);
 
-            match run_file_cleanup(idx_task, &config) {
+            match run_file_cleanup(task_nr, &config) {
                 Ok(_) => {
-                    println!("[{}] Task was successfully executed", idx_task);
+                    println!("[{}] Task was successfully executed", task_nr);
+                    tasks_success += 1;
                 }
                 Err(e) => {
                     eprintln!("[{}] Task error: {}", idx_task, e);
+                    tasks_failure += 1;
                 }
             }
 
-            //Separation for better log readability
-            if idx_task < config.file_list.len() {
-                println!("----------------");
-            }
+            //Log separation for better readability
+            tasks_executed += 1;
+            println!("----------------");
         }
-
-        Ok(())
     }
+
+    //Calculate percentage rates
+    let success_rate: usize = tasks_success * 100 / tasks_executed;
+    let failure_rate: usize = tasks_failure * 100 / tasks_executed;
+
+    //Print task stats
+    println!(
+        "Successful tasks: {}/{} [{}%]",
+        tasks_success, tasks_executed, success_rate
+    );
+    println!(
+        "Failure tasks:    {}/{} [{}%]",
+        tasks_failure, tasks_executed, failure_rate
+    );
+
+    //Log that all tasks have finished
+    println!("All tasks done");
+    Ok(())
 }
 
-fn run_file_cleanup(idx_task: usize, config: &Config) -> Result<(), io::Error> {
+/// Execute a single file cleanup task for a given config
+fn run_file_cleanup(task_nr: usize, config: &Config) -> Result<(), io::Error> {
     Ok(())
 }
